18a00d18cffb1076b257b80fc884e688d441a896
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-27 15:56:00 -0400

    Added CSS handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -14,4 +15,12 @@ module.exports = {
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
